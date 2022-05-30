dfd2055fd3d6f1677e059e704ccb80d844d1ad8b
Filip Szymala <slimymenyt@gmail.com>
2022-05-30 19:46:56 +0200

    Fixed styling issues

--- src/components/PlaceCard.js
+++ src/components/PlaceCard.js
@@ -1,11 +1,11 @@
 import React from "react"
 import locationIcon from "../images/location-icon.svg"
-export default function PlaceCard({imageUrl}) {
+export default function PlaceCard(props) {
     return (
         <div className="card">
             <div className="card__background"
                 style={{
-                    backgroundImage: `url(${imageUrl})`
+                    backgroundImage: `url(${props.imageUrl})`
                 }}
             >
             </div>
@@ -15,7 +15,7 @@ export default function PlaceCard({imageUrl}) {
                     <span className="country-name">{props.location}</span>
                     <a href={props.googleMapsUrl} className="google-maps-link">View on Google Maps</a>
                 </p>
-                <div className="text__info">
+                <div className="card__text__info">
                     <h1>{props.title}</h1>
                     <strong>{props.startDate} - {props.endDate}</strong>
                     <p>{props.description}</p>
